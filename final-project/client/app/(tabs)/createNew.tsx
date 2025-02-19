941778fc65cf31ac2a7b3007d72668990192d551
Lasse Haverinen <lassehav@oamk.fi>
2025-02-19 10:26:28 +0200

    Added Camera view and photo taking.

--- final-project/client/app/(tabs)/createNew.tsx
+++ final-project/client/app/(tabs)/createNew.tsx
@@ -1,10 +1,105 @@
-import { SafeAreaView, View, Text } from 'react-native'
-import React from 'react'
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Pressable, Image } from 'react-native';
 
-export default function createNew() {
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const ref = useRef<CameraView>(null);
+  const [uriToTakenPhoto, setUriToTakenPhoto] = useState<string | null>(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const takePicture = async () => {
+    const photo = await ref.current?.takePictureAsync();
+    setUriToTakenPhoto(photo?.uri);
+  };
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  function renderCameraView() {
+    return (<CameraView style={styles.camera} facing={facing} ref={ref}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <Pressable onPress={() => takePicture()}>
+            <View style={ styles.takePictureButton }></View>
+          </Pressable>
+        </View>
+      </CameraView>);
+  }
+
+  function renderTakenPhoto() {
+    return (
+      <View style={{ width: '100%', alignItems: 'center', justifyContent: 'flex-start', flexDirection: 'column' }}>
+        <Image
+          source={{ uri: uriToTakenPhoto }}
+          contentFit="contain"
+          style={{ width: '100%', aspectRatio: 1 }}
+        />
+        <Pressable onPress={() => setUriToTakenPhoto(null)}>
+          <Text style={{}}>Take another picture</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView>
-      <Text>createNew</Text>
-    </SafeAreaView>
-  )
+    <View style={styles.container}>
+      { uriToTakenPhoto ? renderTakenPhoto() : renderCameraView()}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  takePictureButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: 'red'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    margin: 64,
+    
+  },
+  button: {
+    margin: 20
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
